df92b62a118d77c4edd780b15d9c5e188ce1ce2f
KevTaylor1990 <kevt1@hotmail.com>
2022-08-16 10:38:00 +0000

    Add basic functions

---
+++ assets/JavaScript/script.js
@@ -0,0 +1,28 @@
+document.addEventListener ("DOMContentLoaded", function{
+    let buttons = this.getElementById('button');
+})
+
+
+function runGame {
+
+};
+
+function displayYourChoice {
+
+};
+
+function displayComputerChoice {
+
+};
+
+function displayYourScore {
+
+};
+
+function displayComputerScore{
+
+};
+
+function displayResults {
+
+};
