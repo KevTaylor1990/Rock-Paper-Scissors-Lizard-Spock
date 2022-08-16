83ee5ea4c00c5f1378caab5c94c94a40fd53110b
KevTaylor1990 <kevt1@hotmail.com>
2022-08-16 11:18:09 +0000

    Added rules to the game

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -2,12 +2,57 @@ document.addEventListener ("DOMContentLoaded", function{
     let buttons = this.getElementById('button');
 })
 
+buttons.addEventListener('click')
+
+let gameRules = {
+    Rock: {
+        Rock: 'Draw',
+        Paper: 'Lose',
+        Scissors: 'Win',
+        Lizard: 'Win',
+        Spock: 'Lose'
+    },
+
+    Paper: {
+        Rock: 'Win',
+        Paper: 'Draw',
+        Scissors: 'Lose',
+        Lizard: 'Lose',
+        Spock: 'Win'
+    },
+
+    Scissors:{
+        Rock: 'Lose',
+        Paper: 'Win',
+        Scissors: 'Draw',
+        Lizard: 'win',
+        Spock: 'Lose'
+    },
+
+    Lizard: {
+        Rock: 'Lose',
+        Paper: 'Win',
+        Scissors: 'Lose',
+        Lizard: 'Draw',
+        Spock: 'Win'
+    },
+
+    Spock: {
+        Rock: 'Lose',
+        Paper: 'Win',
+        Scissors: 'Lose',
+        Lizard: 'Lose',
+        Spock: 'Draw'
+    },
+}
 
 function runGame {
 
 };
 
 function displayYourChoice {
+
+
 
 };
 
