2cab9a40088173cdd325be2267041a374358ff24
KevTaylor1990 <kevt1@hotmail.com>
2022-08-18 13:22:39 +0000

    made changes shown by tutor help

---
+++ assets/JavaScript/Script.js
@@ -0,0 +1,81 @@
+var computerChoiceDisplay = document.getElementById("computer-choice");
+var yourChoiceDisplay = document.getElementById("your-choice");
+var resultsDisplay = document.getElementById("results");
+var choices = document.querySelectorAll("button");
+
+var yourChoice;
+var computersChoice;
+var results;
+
+choices.forEach(choices => choices.addEventListener('click', function(event) {
+    yourChoice = event.target.id;
+    yourChoiceDisplay.innerHTML = yourChoice;
+    generateComputerChoice();
+    getResult();
+}));
+
+function generateComputerChoice() {
+    var randomNumber = Math.floor(Math.random() * 5) +1;
+
+    if (randomNumber === 1); {
+        computersChoice = "Rock";
+    } if (randomNumber === 2) {
+        computersChoice = "Paper";
+    } else if (randomNumber === 3) {
+        computersChoice = "Scissors";
+    } else if (randomNumber === 4) {
+        computersChoice = "Lizard";
+    } else if (randomNumber === 5); {
+        computersChoice = "Spock";
+    }
+
+    computerChoiceDisplay.innerHTML = computersChoice;
+}
+
+function getResult() {
+    if (computersChoice === yourChoice) {
+      results = 'its a draw!';
+    } else if (computersChoice === 'rock' && yourChoice === "paper"); {
+      results = 'you win!';
+    } if (computersChoice === 'rock' && yourChoice === "spock") {
+        results = 'you win!';
+    }  else if (computersChoice === 'scissors' && yourChoice === "spock") {
+        results = 'you win!';
+    }  else if (computersChoice === 'scissors' && yourChoice === "rock") {
+        results = 'you win!';
+    }  else if (computersChoice === 'paper' && yourChoice === "lizard") {
+        results = 'you win!';
+    }  else if (computersChoice === 'paper' && yourChoice === "scissors") {
+        results = 'you win!';
+    }  else if (computersChoice === 'lizard' && yourChoice === "rock") {
+        results = 'you win!';
+    }  else if (computersChoice === 'lizard' && yourChoice === "scissors") {
+        results = 'you win!';
+    }  else if (computersChoice === 'spock' && yourChoice === "lizard") {
+        results = 'you win!';
+    }  else if (computersChoice === 'spock' && yourChoice === "paper") {
+        results = 'you win!';
+    }  else if (computersChoice === 'rock' && yourChoice === 'scissors'){
+        results = 'you lose!';
+    }  else if (computersChoice === 'rock' && yourChoice === 'lizard') {
+        results = 'you lose!';
+    }  else if (computersChoice === 'paper' && yourChoice === 'rock') {
+        results = 'you lose!';
+    }  else if (computersChoice === 'paper' && yourChoice === 'spock') {
+        results = 'you lose!';
+    }  else if (computersChoice === 'scissors' && yourChoice === 'lizard') {
+        results = 'you lose!';
+    }  else if (computersChoice === 'scissors' && yourChoice === 'paper') {
+        results = 'you lose!';
+    }  else if (computersChoice === 'lizard' && yourChoice === 'paper') {
+        results = 'you lose!';
+    }  else if (computersChoice === 'lizard' && yourChoice === 'spock') {
+        results = 'you lose!';
+    }  else if (computersChoice === 'spock' && yourChoice === 'scissors') {
+        results = 'you lose!';
+    }  else if (computersChoice === 'spock' && yourChoice === 'rock') {
+        results = 'you lose!';
+    }
+
+    resultsDisplay.innerHTML = results;
+}
